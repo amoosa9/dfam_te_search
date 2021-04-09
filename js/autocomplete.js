0f1210b4a1fbf81dd6e99f6f3f5e8a7cc919e80c
Amer Moosa <amoosa1@jhu.edu>
2021-04-09 18:15:33 -0400

    Fixed autocomplete JavaScript

--- js/autocomplete.js
+++ js/autocomplete.js
@@ -2,8 +2,25 @@
 window.onload=function() {
 
    $("#search_term").autocomplete({
+
       //gets possible values by searching database
-      source: "./autocomplete_te.cgi",
+      source: function(request, response) {
+
+	//queries database
+	$.ajax({
+
+		url: "./autocomplete_te.cgi",
+		type: 'get',
+		dataType: "json",
+		data: {
+		   search_term: request.term
+		},
+		success: function(data) {
+		   response(data);
+		}
+	});
+
+      },
 	
       //the minimum number of characters needed to trigger autocomplete
       minLength: 2,
